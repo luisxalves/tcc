4cb6e1f743171edd08985075c606878c5c652d96
Luís Carlos Alves de Oliveira <luisxalves@hotmail.com>
2017-05-19 11:41:55 -0300

    Delete db_config.php

--- db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define ('DB_USER', "u885774599_luis"); //usuario do banco de dados
-define ('DB_PASSWORD', "210685"); //senha do banco de dados
-define ('DB_DATABASE',"u885774599_tcc"); //nome do banco de dados
-define ('DB_SERVER', "localhost"); // servidor do banco de dados
-?>
